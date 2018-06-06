f09cca87e18ba2626bd8f8b4a5f085e8919f145f
admin <admin@admin-PC>
2018-06-06 10:17:48 +0800

    fvfvvc

---
+++ src/controller/Tantan.java
@@ -0,0 +1,14 @@
+package controller;
+
+public class Tantan {
+	 public static void main(String[] args){
+		 String a="wo shi yi ge shi jie de zhu zhai";
+				 String[] b= a.split(" ");
+				 for (int i = b.length-1; i >=0; i--) {
+					System.out.println(b[i]);
+				}
+		 System.out.println("woainisdsdsd");
+//		 System.out.println("");
+	 }
+
+}
